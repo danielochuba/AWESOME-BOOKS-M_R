51ec6eeaf85f653a7d2dc8a039d52f747a58a6f4
Daniel Ochuba <danielochuba78@gmail.com>
2023-04-26 08:58:49 +0100

    Add filter method

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const authorInput = document.getElementById('author-input');
 
 const addBtn = document.querySelector('.add-btn');
 
-const Books = JSON.parse(localStorage.getItem('books')) || [];
+let Books = JSON.parse(localStorage.getItem('books')) || [];
 
 addBtn.addEventListener('click', () => {
   const titleValue = titleInput.value;
@@ -24,12 +24,11 @@ function addBook(title, author, id) {
   };
   Books.push(book);
   localStorage.setItem('books', JSON.stringify(Books));
-  // eslint-disable-next-line no-use-before-define
   displayBooks();
 }
 
-function removeBook(index) {
-  Books.splice(index, 1);
+function removeBook(id) {
+  Books = Books.filter((book) => book.ID !== id);
   localStorage.setItem('books', JSON.stringify(Books));
   displayBooks();
 }
@@ -37,43 +36,25 @@ function removeBook(index) {
 function displayBooks() {
   booksContainer.innerHTML = ''; // Clear the books container before rendering new books
   Books.forEach((book) => {
-    booksContainer.innerHTML += `
-        <div id="${book.ID}">
-            <p class="book-title">${book.title} <br>${book.author} </p>
-            <button class="delete-btn" id="${book.ID}">Remove</button>
-        </div>
-        <hr>
-        `;
-    booksContainer.innerHTML = '';
-    Books.forEach((book) => {
-      const div = document.createElement('div');
-      div.id = book.id;
+    const div = document.createElement('div');
+    div.id = book.ID;
 
-      const p = document.createElement('p');
-      p.className = 'book-title';
-      p.innerHTML = `${book.title} <br> ${book.author}`;
+    const p = document.createElement('p');
+    p.className = 'book-title';
+    p.innerHTML = `${book.title} <br> ${book.author}`;
 
-      const btn = document.createElement('button');
-      btn.className = 'delete-btn';
-      btn.innerHTML = 'Remove';
-      btn.id = book.id;
-      btn.addEventListener('click', () => {
-        removeBook(Books.indexOf(book));
-      });
-
-      div.appendChild(p);
-      div.appendChild(btn);
-
-      booksContainer.appendChild(div);
-      booksContainer.appendChild(document.createElement('hr'));
+    const btn = document.createElement('button');
+    btn.className = 'delete-btn';
+    btn.innerHTML = 'Remove';
+    btn.id = book.ID;
+    btn.addEventListener('click', () => {
+      removeBook(book.ID);
     });
 
-    // Add event listeners to the remove buttons after they are rendered
-    const removeBtns = document.querySelectorAll('.delete-btn');
-    removeBtns.forEach((btn, index) => {
-      btn.addEventListener('click', () => {
-        removeBook(index);
-      });
-    });
+    div.appendChild(p);
+    div.appendChild(btn);
+
+    booksContainer.appendChild(div);
+    booksContainer.appendChild(document.createElement('hr'));
   });
 }
