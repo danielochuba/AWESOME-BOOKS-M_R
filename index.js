f4afbc7b8f58c47988ee2c1c59c756e9363541f0
Jrmcuervo <jrmcuervom@gmail.com>
2023-04-26 08:45:02 -0600

    refactor with js classes

--- index.js
+++ index.js
@@ -1,79 +1,63 @@
-/* eslint-disable no-use-before-define */
-const booksContainer = document.querySelector('.books');
-const titleInput = document.getElementById('title-input');
-const authorInput = document.getElementById('author-input');
-
-const addBtn = document.querySelector('.add-btn');
-
-const Books = JSON.parse(localStorage.getItem('books')) || [];
-
-addBtn.addEventListener('click', () => {
-  const titleValue = titleInput.value;
-  const authorValue = authorInput.value;
-  const bookID = Books.length;
-  addBook(titleValue, authorValue, bookID);
-});
-
-displayBooks();
-
-function addBook(title, author, id) {
-  const book = {
-    title,
-    author,
-    ID: id,
-  };
-  Books.push(book);
-  localStorage.setItem('books', JSON.stringify(Books));
-  // eslint-disable-next-line no-use-before-define
-  displayBooks();
+/*eslint-disable no-unused-vars
+eslint-disable max-classes-per-file*/
+class Book {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
 }
-
-function removeBook(index) {
-  Books.splice(index, 1);
-  localStorage.setItem('books', JSON.stringify(Books));
-  displayBooks();
-}
-
-function displayBooks() {
-  booksContainer.innerHTML = ''; // Clear the books container before rendering new books
-  Books.forEach((book) => {
-    booksContainer.innerHTML += `
-        <div id="${book.ID}">
-            <p class="book-title">${book.title} <br>${book.author} </p>
-            <button class="delete-btn" id="${book.ID}">Remove</button>
-        </div>
-        <hr>
-        `;
-    booksContainer.innerHTML = '';
-    Books.forEach((book) => {
+class Library {
+  constructor(container, titleInput, authorInput, addBtn) {
+    this.booksContainer = document.querySelector(container);
+    this.titleInput = document.getElementById(titleInput);
+    this.authorInput = document.getElementById(authorInput);
+    this.addBtn = document.querySelector(addBtn);
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.displayBooks();
+    this.addEventListeners();
+  }
+  addBook(title, author, id) {
+    const book = new Book(title, author, id);
+    this.books.push(book);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+  removeBook(index) {
+    this.books.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+  displayBooks() {
+    this.booksContainer.innerHTML = '';
+    this.books.forEach((book) => {
       const div = document.createElement('div');
       div.id = book.id;
-
       const p = document.createElement('p');
       p.className = 'book-title';
       p.innerHTML = `${book.title} <br> ${book.author}`;
-
       const btn = document.createElement('button');
       btn.className = 'delete-btn';
       btn.innerHTML = 'Remove';
       btn.id = book.id;
       btn.addEventListener('click', () => {
-        removeBook(Books.indexOf(book));
+        this.removeBook(this.books.indexOf(book));
       });
-
       div.appendChild(p);
       div.appendChild(btn);
-
-      booksContainer.appendChild(div);
-      booksContainer.appendChild(document.createElement('hr'));
+      this.booksContainer.appendChild(div);
+      this.booksContainer.appendChild(document.createElement('hr'));
     });
-
-    // Add event listeners to the remove buttons after they are rendered
-    const removeBtns = document.querySelectorAll('.delete-btn');
-    removeBtns.forEach((btn, index) => {
-      btn.addEventListener('click', () => {
-        removeBook(index);
-      });
+  }
+  addEventListeners() {
+    this.addBtn.addEventListener('click', () => {
+      if (this.titleInput.value !== '' && this.authorInput.value !== '') {
+        const titleValue = this.titleInput.value;
+        const authorValue = this.authorInput.value;
+        const bookID = Date.now();
+        this.addBook(titleValue, authorValue, bookID);
+      }
     });
-  });
+  }
 }
+const library = new Library('.books', 'title-input', 'author-input', '.add-btn');
